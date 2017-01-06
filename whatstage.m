630aabcec9e506f9f80d201350d4240cb952ebc5
rrrventures <imauriacaf@gmail.com>
2017-01-06 17:18:06 -0300

    Update whatstage.m

--- whatstage.m
+++ whatstage.m
@@ -1,12 +1,12 @@
 function stage = whatstage(image)
-	%Import the model. I should probably avoid loading it each time
+	%Import the model. Remember to avoid loading it each time
 	categoryClassifier = importdata('C:/py/stageClassifer.mat');
 
 	%Stage identifier function
 	%Crop and resize frame
 	bboxI = [367 3 912 715];
 	image=imcrop(image,bboxI);
-    image=imresize(image,0.4);
+    	image=imresize(image,0.4);
 
     %predict stage
 	[labelIdx, scores] = predict(categoryClassifier, image);
